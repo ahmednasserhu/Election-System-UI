2b8c6e37e080ae4b1b3fb003ac1a77479074425e
DevMAyman <eng.developer.mohamed.ayman@gmail.com>
2024-06-21 05:20:23 +0300

    Finial home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './general/home/home.component';
+import { AboutUsComponent } from './general/about-us/about-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutUsComponent,
+  },
+];
