14a29842b3fcb98651ebf9c3bf4baf8fdb61de83
fatmaelhot <fatmaelhot289@gmail.com>
2024-06-25 01:20:24 +0300

    add home page of admin

--- src/app/Admin/Components/home/home.component.ts
+++ src/app/Admin/Components/home/home.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { VoteService } from '../../Services/vote.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  currentDate = new Date();
+  totalVoters = 120000;
+  registeredCandidates = 150;
+  votesCast = 11;
+  pendingIssues = 24;
+  recentActivities = [
+    { timestamp: new Date(), description: 'Candidate A registered' },
+    { timestamp: new Date(), description: 'Voter turnout update posted' },
+    { timestamp: new Date(), description: 'New voting center added' }
+  ];
+  constructor(private voteService: VoteService) { }
+ 
+  fetchTotalVotes(): void {
+    this.voteService.getTotalVotes().subscribe(response => {
+      this.votesCast = response.total;
+      console.log(response);
+      
+    });
+  }
+  
 }
--- src/app/Admin/Services/vote.service.ts
+++ src/app/Admin/Services/vote.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface VoteResponse {
+  total: number;
+  votes: any[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class VoteService {
+  private apiUrl = 'http://localhost:3000/votes/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTotalVotes(): Observable<VoteResponse> {
+    return this.http.get<VoteResponse>(this.apiUrl);
+  }
 }
