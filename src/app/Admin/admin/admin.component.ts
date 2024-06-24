3791ffd2652b41c5615a185704e53ebc7c79faeb
fatmaelhot <fatmaelhot289@gmail.com>
2024-06-24 16:47:52 +0300

    handle admin component

--- src/app/Admin/admin/admin.component.ts
+++ src/app/Admin/admin/admin.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../Components/sidebar/sidebar.component';
+import { NavbarComponent } from '../Components/navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent,NavbarComponent,RouterOutlet],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
