1d7cff1d1f60accd88592d651ecd03807d567a7d
fatmaelhot <fatmaelhot289@gmail.com>
2024-06-24 21:32:08 +0300

    add navbar &sidebar of adminboard

--- src/app/Admin/Components/sidebar/sidebar.component.ts
+++ src/app/Admin/Components/sidebar/sidebar.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component ,OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+   isClosed = false;
 
+   menuItems = [
+    { name: 'Home', icon: 'home', route: '/admin' },
+    { name: 'Candidates', icon: 'tachometer', route: '/admin/candidate' },
+    { name: 'Citizen', icon: 'users', route: '/admin/citizen' },
+    { name: 'Election', icon: 'envelope', route: '/admin/election' },
+    
+  ];
+
+toggleSidebar() {
+  this.isClosed = !this.isClosed;
+}
 }
