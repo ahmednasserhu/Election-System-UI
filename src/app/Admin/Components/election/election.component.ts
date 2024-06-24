db749530ff9fbb0d06620d5a6188b662765eb3ea
fatmaelhot <fatmaelhot289@gmail.com>
2024-06-24 16:40:41 +0300

    intial structure of admin dashboard

---
+++ src/app/Admin/Components/election/election.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-election',
+  standalone: true,
+  imports: [],
+  templateUrl: './election.component.html',
+  styleUrl: './election.component.css'
+})
+export class ElectionComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './Admin/admin/admin.component';
+import { HomeComponent } from './Admin/Components/home/home.component';
+import { CandidateComponent } from './Admin/Components/candidate/candidate.component';
+import { ElectionComponent } from './Admin/Components/election/election.component';
+import { CitizenComponent } from './Admin/Components/citizen/citizen.component';
+import { PageNotFoundComponent } from './Admin/Components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: "admin",
+    component: AdminComponent,
+    // canActivate: [AuthGuard],
+
+    children: [
+        { path: "", component: HomeComponent },
+        { path: "candidate", component: CandidateComponent },
+        { path: "election", component: ElectionComponent },
+        { path: "citizen", component: CitizenComponent },
+       
+        { path: "**", component: PageNotFoundComponent },
+    
+    ],
+}
+];
