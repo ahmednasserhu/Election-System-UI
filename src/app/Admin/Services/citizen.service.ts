063bf5c8055086b27eb8d58466393476ce910792
fatmaelhot <fatmaelhot289@gmail.com>
2024-06-25 05:51:09 +0300

    add citizen management page of admin and integration

--- src/app/Admin/Services/citizen.service.ts
+++ src/app/Admin/Services/citizen.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient ,HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Citizen } from '../Interfaces/citizen';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CitizenService {
   private apiUrl = 'http://localhost:3000/citizens/';
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
   constructor(private http: HttpClient) { }
 
   getTotalCitizen(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
+  }
+  getCitizens(): Observable<Citizen[]> {
+    return this.http.get<Citizen[]>(this.apiUrl);
+  }
+
+  blockCitizen(citizenId: number): Observable<any> {
+    const url = `${this.apiUrl}${citizenId}/status`;
+    return this.http.put(url, { status: 'Blocked' }, this.httpOptions);
   }
 }
