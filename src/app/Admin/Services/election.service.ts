2db3d92d83e2521648c068bacee60ab34c05503f
fatmaelhot <fatmaelhot289@gmail.com>
2024-06-25 06:17:42 +0300

    add election management page of admin with crud operations  and integration

--- src/app/Admin/Services/election.service.ts
+++ src/app/Admin/Services/election.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Election } from '../Interfaces/election';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,24 @@ export class ElectionService {
 
   getLastElection(): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}last-election`);
+  }
+  getElections(): Observable<Election[]> {
+    return this.http.get<Election[]>(this.apiUrl);
+  }
+
+  getElection(id: number): Observable<Election> {
+    return this.http.get<Election>(`${this.apiUrl}/${id}`);
+  }
+
+  createElection(election: Election): Observable<Election> {
+    return this.http.post<Election>(this.apiUrl, election);
+  }
+
+  updateElection(election: Election): Observable<Election> {
+    return this.http.put<Election>(`${this.apiUrl}/${election.id}`, election);
+  }
+
+  deleteElection(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { AdminComponent } from './Admin/admin/admin.component';
 import { HomeComponent } from './Admin/Components/home/home.component';
-import { CandidateComponent } from './Admin/Components/candidate/candidate.component';
+import { CandidateComponent } from './Admin/Components/candidate/candidate.component'
 import { ElectionComponent } from './Admin/Components/election/election.component';
 import { CitizenComponent } from './Admin/Components/citizen/citizen.component';
 import { PageNotFoundComponent } from './Admin/Components/page-not-found/page-not-found.component';
