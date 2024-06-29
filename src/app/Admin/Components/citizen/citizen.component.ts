e5a178b37e54c4218f1f4f87459f310fdb219d4d
fatmaelhot <fatmaelhot289@gmail.com>
2024-06-29 18:54:39 +0300

    add pagination of citizens

--- src/app/Admin/Components/citizen/citizen.component.ts
+++ src/app/Admin/Components/citizen/citizen.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Citizen } from '../../Interfaces/citizen';
 import { CitizenService } from '../../Services/citizen.service';
 import { CommonModule } from '@angular/common';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-citizen',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgxPaginationModule],
   templateUrl: './citizen.component.html',
   styleUrls: ['./citizen.component.css'],
 })
@@ -14,6 +15,8 @@ export class CitizenComponent implements OnInit {
   citizens: Citizen[] = [];
   blockedCitizens: Citizen[] = [];
   error: string = '';
+  page: number = 1; // Current page for citizens
+  blockedPage: number = 1; // Current page for blocked citizens
 
   constructor(private citizenService: CitizenService) {}
 
@@ -37,9 +40,7 @@ export class CitizenComponent implements OnInit {
   loadBlockedCitizens(): void {
     this.citizenService.getCitizens().subscribe(
       (data) => {
-        this.blockedCitizens = data.filter(
-          (citizen) => citizen.status === 'blocked',
-        );
+        this.blockedCitizens = data.filter((citizen) => citizen.status === 'blocked');
       },
       (error) => {
         this.error = 'Error loading blocked citizens: ' + error.message;
@@ -49,14 +50,14 @@ export class CitizenComponent implements OnInit {
   }
 
   blockCitizen(citizen: Citizen, action: 'blocked' | 'unblocked'): void {
-    let statusToUpdate: string = ''; // Initialize with a default value
+    let statusToUpdate: string = '';
 
     if (action === 'blocked') {
       statusToUpdate = 'blocked';
     } else if (action === 'unblocked') {
       statusToUpdate = 'unblocked';
     } else {
-      return; // Handle unexpected actions gracefully
+      return;
     }
 
     this.citizenService
